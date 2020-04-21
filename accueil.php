71c70e7f72b127d1038e4e0291ab2fabb1f0e239
flnh <flnh57@gmail.com>
2020-04-21 14:39:44 +0200

    selected

--- accueil.php
+++ accueil.php
@@ -28,5 +28,10 @@
             ?>
         </div>
     </div>
+    <div class="row">
+        <div class="col">
+            
+        </div>
+    </div>
 </body>
 </html>
--- views/listeGroupes.php
+++ views/listeGroupes.php
@@ -1,9 +1,12 @@
 <?php
 function afficherListeGroupes(array $listeGroupes) {
   for ($i=0; $i < count($listeGroupes); $i++) {
-    // echo '<option value="'. $donnees["$nom_index"]. '">' . $donnees["$content"] .' </option>';
+    $selected = "";
+    if (isset($_GET['liste_groupe']) && $_GET['liste_groupe'] == $i + 1) {
+      $selected = "selected";
+    }
     ?>
-    <option value="<?=$listeGroupes[$i]->getId_groupe()?>"><?=$listeGroupes[$i]->getNom_groupe()?></option>
+    <option value="<?=$listeGroupes[$i]->getId_groupe()?>" <?=$selected?>><?=$listeGroupes[$i]->getNom_groupe()?></option>
     <?php
   }
 }
